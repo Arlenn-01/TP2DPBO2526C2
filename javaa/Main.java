f0c22b92c4b90da04a1d459ef76e70003f69162f
Arlenn <renaldiarlenpurbaa@gmail.com>
2026-09-25 03:59:04 +0700

    feat: membuat class Manusia berisi method informasi dan class Main untuk test sederhana

---
+++ javaa/Main.java
@@ -0,0 +1,8 @@
+package javaa;
+
+public class Main {
+    public static void main(String[] args) {
+        Manusia person = new Manusia("Arlen", 20, "Pria");
+        person.informasi();
+    }
+}
