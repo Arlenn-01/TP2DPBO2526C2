3a1e8662bbc2c30ab3f2b6be081441b50a391f18
Arlenn <renaldiarlenpurbaa@gmail.com>
2026-09-26 21:26:48 +0700

    refactor: mengubah atribut dari public ke private

--- pythonn/Manusia.py
+++ pythonn/Manusia.py
@@ -4,23 +4,23 @@ class Manusia :
     def __init__(self, nama, umur, gender) : 
         self.setNama(nama) #harus pakai prefix self kalau scopenya sama satu class
         self.setUmur(umur)
-        self.gender = gender
+        self.__gender = gender
 
     #setter
     def setNama(self, nama) :
         if (nama == "") : 
             print("Nama tidak boleh kosong wok")
             return #early exit
-        self.nama = nama
+        self.__nama = nama
 
     def setUmur(self, umur) :
         if (umur <= 0) :
             print("Masukkan umur dengan benar")
             return 
-        self.umur = umur
+        self.__umur = umur
 
     def setGender(self, gender) :
-        self.gender = gender
+        self.__gender = gender
 
     #getter
     def getNama(self) :
